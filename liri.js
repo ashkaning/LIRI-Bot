732e2ed77678b0420db783dc59bc5f1a4363757a
ashkan <ashkan.ram70@gmail.com>
2019-08-19 18:23:34 -0700

    fixed some bugs and added some functions

--- liri.js
+++ liri.js
@@ -7,52 +7,8 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var funcSearch = process.argv[2];
 var KeySearch = process.argv[3]
-if (funcSearch === 'do-what-it-says') {
-    fs.readFile('random.txt', 'utf8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        var dataArr = data.split(",");
-        if (dataArr[0] === 'spotify-this-song') {
-            var KeySearch = dataArr[1]
-            song(KeySearch)
-            //console.log('hi')
-        }
-        if (dataArr[0] === 'concert-this') {
-            var KeySearch = dataArr[1]
-            concertFunc(KeySearch)
-        }
-
-    })
-
-}
-
 
 var movieUrl = "http://www.omdbapi.com/?t=" + KeySearch + "&y=&plot=short&apikey=trilogy";
-
-
-if (funcSearch === 'concert-this') {
-    var concertUrl = "https://rest.bandsintown.com/artists/" + KeySearch + "/events?app_id=codingbootcamp";
-    axios.get(concertUrl).then(
-        function (concertResponse) {
-            concert(concertResponse)
-
-        }).catch(function (error) {
-            console.log(error);
-        })
-}
-if (funcSearch === 'spotify-this-song') {
-    song(KeySearch)
-}
-
-if (funcSearch === 'movie-this') {
-    axios.get(movieUrl).then((response) => {
-        movie(response)
-    }).catch((err) => {
-        console.log(err);
-    })
-}
-
 
 //////////////////////////////////
 ////////////////functions////////
@@ -97,7 +53,6 @@ var song = function (KeySearch) {
             console.log('Album: ' + data.tracks.items[i].album.artists[0].name);
             console.log('====================================');
         }
-        // song(data)
     });
 
 }
@@ -113,4 +68,56 @@ var movie = function (response) {
     console.log("Actors in the movie: " + response.data.Actors);
     console.log('====================================');
 }
-//////////////////////////////////////////
+//////////////////////////////////
+////////////////If Conditions////////
+//////////////////////////////////
+if (funcSearch === 'do-what-it-says') {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var dataArr = data.split(",");
+        if (dataArr[0] === 'spotify-this-song') {
+            var KeySearch = dataArr[1]
+            song(KeySearch)
+            //console.log('hi')
+        }
+        if (dataArr[0] === 'concert-this') {
+            var KeySearch = dataArr[1]
+            concertFunc(KeySearch)
+        }
+        if (dataArr[0] === 'movie-this') {
+            var KeySearch = dataArr[1]
+            var movieUrl = "http://www.omdbapi.com/?t=" + KeySearch + "&y=&plot=short&apikey=trilogy";
+            axios.get(movieUrl).then((response) => {
+                movie(response)
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+
+    })
+
+}
+if (funcSearch === 'concert-this') {
+    var concertUrl = "https://rest.bandsintown.com/artists/" + KeySearch + "/events?app_id=codingbootcamp";
+    axios.get(concertUrl).then(
+        function (concertResponse) {
+            concert(concertResponse)
+
+        }).catch(function (error) {
+            console.log(error);
+        })
+}
+if (funcSearch === 'spotify-this-song') {
+    //console.log(KeySearch)
+    song(KeySearch)
+}
+
+if (funcSearch === 'movie-this') {
+    axios.get(movieUrl).then((response) => {
+        movie(response)
+    }).catch((err) => {
+        console.log(err);
+    })
+}
